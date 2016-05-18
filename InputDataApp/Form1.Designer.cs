fd6e403b5f442c331bb106ee349c40b74f031405
Didik Setiawan <didik.swn@gmail.com>
2016-05-18 10:20:43 +0700

    initial commit
    Signed-off-by: Didik Setiawan <didik.swn@gmail.com>

---
+++ InputDataApp/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace InputDataApp
+{
+    partial class InputDataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tboxUser = new System.Windows.Forms.TextBox();
+            this.tboxIP = new System.Windows.Forms.TextBox();
+            this.tboxKet = new System.Windows.Forms.TextBox();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nama User :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP Address :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Keterangan :";
+            // 
+            // tboxUser
+            // 
+            this.tboxUser.Location = new System.Drawing.Point(99, 11);
+            this.tboxUser.Name = "tboxUser";
+            this.tboxUser.Size = new System.Drawing.Size(100, 20);
+            this.tboxUser.TabIndex = 3;
+            // 
+            // tboxIP
+            // 
+            this.tboxIP.Location = new System.Drawing.Point(99, 34);
+            this.tboxIP.Name = "tboxIP";
+            this.tboxIP.Size = new System.Drawing.Size(100, 20);
+            this.tboxIP.TabIndex = 4;
+            // 
+            // tboxKet
+            // 
+            this.tboxKet.Location = new System.Drawing.Point(99, 57);
+            this.tboxKet.Name = "tboxKet";
+            this.tboxKet.Size = new System.Drawing.Size(100, 20);
+            this.tboxKet.TabIndex = 5;
+            // 
+            // btnCari
+            // 
+            this.btnCari.Location = new System.Drawing.Point(204, 10);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 23);
+            this.btnCari.TabIndex = 6;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(204, 34);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 23);
+            this.btnTambah.TabIndex = 7;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(204, 58);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(204, 82);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 9;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(279, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // InputDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.tboxKet);
+            this.Controls.Add(this.tboxIP);
+            this.Controls.Add(this.tboxUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "InputDataForm";
+            this.Text = "Input Data";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tboxUser;
+        private System.Windows.Forms.TextBox tboxIP;
+        private System.Windows.Forms.TextBox tboxKet;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ InputDataApp/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace InputDataApp
+{
+    public partial class InputDataForm : Form
+    {
+        private static string strconn = "server=.; user id = user; password = sebuahpassword; database=InputDataDB;";
+        private SqlCommand cmd;
+        private SqlDataReader rd;
+
+        public InputDataForm()
+        {
+            InitializeComponent();
+            BindGrid();
+        }
+
+        private void BindGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(strconn))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM datakomputer", conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void awal()
+        {
+            tboxUser.Text = "";
+            tboxIP.Text = "";
+            tboxKet.Text = "";
+        }
+
+        private void InputDataForm_Load(object sender, EventArgs e)
+        {
+            awal();
+            BindGrid();
+        }
+
+        private void btnCari_Click(object sender, EventArgs e)
+        {
+            if (tboxUser.Text.Trim() != "")
+            {
+                using (SqlConnection conn = new SqlConnection(strconn))
+                {
+                    cmd = new SqlCommand("select * from datakomputer where NamaUser='" + tboxUser.Text + "'", conn);
+                    conn.Open();
+                    rd = cmd.ExecuteReader();
+                    rd.Read();
+
+                    if (rd.HasRows)
+                    {
+                        tboxUser.Text = rd["NamaUser"].ToString();
+                        tboxIP.Text = rd["IP_Address"].ToString();
+                        tboxKet.Text = rd["Deskripsi"].ToString();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data komputer tidak ditemukan", "Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            if (tboxUser.Text.Trim() == "" || tboxIP.Text.Trim() == "" || tboxKet.Text.Trim() == "")
+            {
+                MessageBox.Show("Data inputan belum lengkap", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                using (SqlConnection conn = new SqlConnection(strconn))
+                {
+                    cmd = new SqlCommand("insert into datakomputer (NamaUser,IP_Address,Deskripsi) values ('" + tboxUser.Text + "','" + tboxIP.Text + "','" + tboxKet.Text + "')", conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data berhasil disimpan", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    awal();
+                    BindGrid();
+                }
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (tboxUser.Text.Trim() == "" || tboxIP.Text.Trim() == "" || tboxKet.Text.Trim() == "")
+            {
+                MessageBox.Show("Data siswa belum lengkap", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                using (SqlConnection conn = new SqlConnection(strconn))
+                {
+                    cmd = new SqlCommand("update datakomputer set NamaUser='" + tboxUser.Text + "', IP_Address='" + tboxIP.Text + "', Deskripsi='" + tboxKet.Text + "' where NamaUser ='" + tboxUser.Text + "'", conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data berhasil diedit", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    awal();
+                    BindGrid();
+                }
+            }
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(strconn))
+            {
+                cmd = new SqlCommand("delete from datakomputer where NamaUser='" + tboxUser.Text + "'", conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data berhasil dihapus", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                awal();
+                BindGrid();
+            }
+        }
+    }
+}
